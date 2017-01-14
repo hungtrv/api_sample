09359f099b0796e53b860db5ae2a4a9d500c96d1
Hung Tran <hungtrv@gmail.com>
2017-01-14 01:48:01 -0800

    Add @json decorator to ITEMS resource

--- main/controllers/v1/items.py
+++ main/controllers/v1/items.py
@@ -6,31 +6,35 @@ from main import db
 from main.models.orders import Order
 from main.models.items import Item
 
+from main.decorators.json import json
 from flask import jsonify
 from flask import request
 
 
 @api.route('/items/<int:id>', methods=['GET'])
+@json
 def get_item(id):
 	"""
 		Get detail info for a specific item identified by item id
 	"""
 	
-	return jsonify(Item.query.get_or_404(id).export_data())
+	return Item.query.get_or_404(id).export_data()
 	
 
 
 @api.route('/orders/<int:id>/items/', methods=['GET'])
+@json
 def get_order_items(id):
 	""""
 		Get list of all items of a specific order identified by order id
 	"""
 	order = Order.query.get_or_404(id)
 
-	return jsonify({'items': [item.get_url() for item in order.items.all()]})
+	return {'items': [item.get_url() for item in order.items.all()]}
 
 
 @api.route('/orders/<int:id>/items/', methods=['POST'])
+@json
 def add_new_order_item(id):
 	"""
 		Add an item to a specific order identified by order id.
@@ -44,10 +48,11 @@ def add_new_order_item(id):
 	db.session.add(item)
 	db.session.commit()
 
-	return jsonify({}), 201, {'Location': item.get_url()}
+	return {}, 201, {'Location': item.get_url()}
 
 
 @api.route('/items/<int:id>', methods=['PUT'])
+@json
 def edit_item(id):
 	"""
 		Edit a specific item identified by item id
@@ -61,10 +66,11 @@ def edit_item(id):
 	db.session.add(item)
 	db.session.commit()
 
-	return jsonify({})
+	return {}
 
 
 @api.route('/items/<int:id>', methods=['DELETE'])
+@json
 def delete_item(id):
 	"""
 		Delete a specific item identified by item id
@@ -73,5 +79,5 @@ def delete_item(id):
 	db.session.delete(item)
 	db.session.commit()
 
-	return jsonify({})
+	return {}
 
