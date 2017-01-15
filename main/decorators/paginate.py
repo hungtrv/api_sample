44ffad23eae0d9e8415a6035e736011cae346c60
Hung Tran <hungtrv@gmail.com>
2017-01-14 23:56:43 -0800

    Add pagination decorator

---
+++ main/decorators/paginate.py
@@ -0,0 +1,42 @@
+import functools
+from flask import url_for
+from flask import request
+
+def paginate(collection, max_per_page=25):
+	"""
+		Decorator for generating pagination for a collection
+		- Functions that use this decorator must return SQLAlchemy query
+		- Output is Python dictionary with paginated results
+		- Application must turn the results into response object either by 
+		another decorator or using custom response object accepting dictionary
+		input
+	"""
+	def decorator(f):
+		@functools.wraps(f)
+		def wrapped(*args, **kwargs):
+			query = f(*args, **kwargs)
+
+			page = request.args.get('page', 1, type=int)
+			per_page = min(request.args.get('per_page', max_per_page, type=int), max_per_page)
+
+			p = query.pagniate(page, per_page)
+
+			pages = {'page': page, 'per_page': per_page, 'total': p.total, 'pages': p.pages}
+
+			if p.has_prev:
+				pages['prev_url'] = url_for(request.endpoint, page=p.prev_num, per_page=per_page, _external=True, **kwargs)
+			else:
+				pages['prev_url'] = None
+
+			if p.has_next:
+				pages['next_url'] = url_for(request.endpoint, page=p.next_num, per_page=per_page, _external=True, **kwargs)
+			else:
+				pages['next_url'] = None
+
+			pages['first_url'] = url_for(request.endpoint, page=1, per_page=per_page, _external=True, **kwargs)
+
+			pages['last_url'] = url_for(request.endpoint, page=1, per_page=p.pages, _external=True, **kwargs)
+
+			return {collection: [item.get_url() for item in p.items], 'pages': pages}
+		return wrapped
+	return decorator
